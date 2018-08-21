f64cacf849005fef3fde4d988487bc0e6f3b3d30
jh <jh_personal@163.com>
2018-08-21 17:53:49 +0800

    换到windows环境下发现一个小问题

--- src/utils/interceptor.js
+++ src/utils/interceptor.js
@@ -2,10 +2,10 @@ import axios from 'axios'
 import {Message, MessageBox} from 'element-ui'
 import store from '../store'
 import router from '../router'
-// 创建axios实例
+
 const service = axios.create({
-  baseURL: process.env.BASE_URL, // api的base_url
-  timeout: 15000                  // 请求超时时间2
+  baseURL: process.env.BASE_URL, 
+  timeout: 15000                  
 })
 // request拦截器
 service.interceptors.request.use(config => {
@@ -20,39 +20,40 @@ service.interceptors.response.use(
   response => {
     const res = response.data;
     //console.log(res.returnCode);
-    if(res.returnCode==="001")
-    {
-      Message({
-        showClose: true,
-        message: res.returnMsg,
-        type: 'error',
-        duration: 500,
-        onClose: () => {
-          store.dispatch('NoLogIn').then(() => {
-            location.reload()// 为了重新实例化vue-router对象 避免bug
-          })
-        }
-      });
-      return Promise.reject("未登录")
-    }else if(res.returnCode==="403"){
-      Message({
-        showClose: true,
-        message: res.returnMsg,
-        type: 'error',
-        duration: 1000,
-        onClose: () => {
-          router.push({path: '/'})
-        }
-      });
-      return Promise.reject("无权限")
-    }
-    //console.log(res.returnData);
-    if(res.returnData){
-      return res.returnData;
+    if(res.returnCode!=="200"){
+      if(res.returnCode==="001")
+      {
+        Message({
+          showClose: true,
+          message: res.returnMsg,
+          type: 'error',
+          duration: 500,
+          onClose: () => {
+            store.dispatch('NoLogIn').then(() => {
+              location.reload()// 为了重新实例化vue-router对象 避免bug
+            })
+          }
+        });
+        return Promise.reject("未登录")
+      }else if(res.returnCode==="403"){
+        Message({
+          showClose: true,
+          message: res.returnMsg,
+          type: 'error',
+          duration: 1000,
+          onClose: () => {
+            router.push({path: '/'})
+          }
+        });
+        return Promise.reject()
+      }
     }else{
-      return res;
+      if(res.returnData){
+        return res.returnData;
+      }else{
+        return res;
+      }
     }
-    
   },
   error => {
     console.error('err' + error)// for debug
